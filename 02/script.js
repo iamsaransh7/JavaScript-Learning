0cb5323608944951b5fc8c1a4f525fbade15a4e7
Saransh Upadhyay <iamsaransh7@gmail.com>
2025-02-07 13:32:08 +0530

    Variables in JS
    var, let, const

---
+++ 02/script.js
@@ -0,0 +1,21 @@
+// var
+if(true){
+    var a = 7;
+}
+
+console.log(a);
+
+
+// temp
+if(true){
+    let b = 9;
+    console.log(b);
+}
+
+// const
+const c = 11;
+// c = 13;
+console.log(c + 2);
+console.log(c);
+// c = c+3;
+// console.log(c);
