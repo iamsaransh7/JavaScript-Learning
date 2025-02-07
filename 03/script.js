51bc9b9d967fb329599ce298e5fb9e15bc40fd40
Saransh Upadhyay <iamsaransh7@gmail.com>
2025-02-07 17:23:20 +0530

    Data Types & Operators in JavaScript

---
+++ 03/script.js
@@ -0,0 +1,36 @@
+// Data Types
+
+// Numbers
+let num1 = 10;
+let num2 = 7;
+console.log("Sum of numbers:", num1 + num2);
+
+// Strings
+const firstName = "Saransh";
+const lastName = 'Upadhyay';
+console.log("Name:", firstName + ' ' + lastName);
+
+// Booleans
+let isLoggedIn = true;
+console.log("Boolean:", true);
+
+// NULL
+let lastLoginDate = null;
+// lastLoginDate = "today";
+console.log(lastLoginDate);
+
+// Undefined
+let lastLogoutDate = undefined;
+// lastLogoutDate = "yesterday";
+console.log(lastLogoutDate);
+
+// Objects
+const personObj = {
+    firstName: "Saransh",
+    lastName: "Upadhyay",
+    age: "28",
+    isLoggedIn: false,
+    lastLoginDate: null
+}
+
+console.log(personObj); 
