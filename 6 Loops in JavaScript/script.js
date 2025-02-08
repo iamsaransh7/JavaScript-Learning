c6988143f7e40777c21a4b3fe86c76b0c47b28db
Saransh Upadhyay <iamsaransh7@gmail.com>
2025-02-08 18:20:09 +0530

    Loops in JavaScript

---
+++ 6 Loops in JavaScript/script.js
@@ -0,0 +1,19 @@
+// FOR Loop
+for(let i=0; i<10; i++){
+    console.log(i + " Saransh")
+}
+
+// WHILE Loop
+let ip = 0;
+let house = 50;
+
+while(ip != 50){
+    ip = ip + 1;
+    console.log('Steps Taken' + ip);
+}
+
+// DO WHILE Loop
+do{
+    ip = ip + 1;
+    console.log(ip);
+} while(ip <= house)
