c3262418ea123b7bf647a6dfafc3d8a267e586f3
Saransh Upadhyay <iamsaransh7@gmail.com>
2025-02-11 18:34:47 +0530

    Arrays in JavaScript

---
+++ 10 Arrays in JavaScript/script.js
@@ -0,0 +1,12 @@
+const students = ["John", "Saransh", "Sai", "Manjunath"];
+console.log(students.length);
+students.push("Deepali");
+students.push("Alexa");
+students.push("Siri");
+console.log(students.length);
+
+students.pop();
+console.log(students);
+
+const myArray = [1, "Saransh", true];
+console.log(myArray);
